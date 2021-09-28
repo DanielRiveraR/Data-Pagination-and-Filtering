ed3464e8191675ab4ca51a2319603fef4a068090
DanielRiveraR <dani_wooten@hotmail.com>
2021-09-28 10:50:56 -0700

    Added comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 /*
-Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 let numStudentsDisplay = 9;
@@ -11,49 +10,52 @@ function showPage(list, page) {
    studentList.innerHTML = '';
 
    for ( let i = 0; i < list.lenght; i++) {
-      const studentItem = `
-         <li class="student-item cf">
-            <div class="student-details">
-            <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
-            <h3>${list[i].name.first} ${list[i].name.last}</h3>
-            <span class="email">${list[i].email}</span>
-            </div>
-            <div class="joined-details">
-            <span class="date">${list[i].registered.date}</span>
-            </div>
-         </li>
-      `;
-
       if (i >= startIndex && i < endIndex) {
+         let studentItem = `
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+                  <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                  <span class="email">${list[i].email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">${list[i].registered.date}</span>
+               </div>
+            </li>
+         `;
          studentList.insertAdjacentHTML('beforeend', studentItem);
       }
    }
 }
 
-showPage(data, 1);
-
-
 /*
-Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 function addPagination(list) {
-   const numOfPage = Math.ceil(list.length / numStudentsDisplay);
+   let numOfPage = Math.ceil(list.length / numStudentsDisplay);
    const linkList = document.querySelector('.link-list');
    linkList.innerHTML = '';
 
    for (let i = 1; i <= numOfPage; i++) {
-      const button = `
+      let button = `
       <li>
          <button type="button">${i}</button>
       </li>
       `;
-      linkList.insertAdjacentElement('beforeend', button);
-      const firstButton = document.querySelector('button');
-      firstButton.className = 'active';
+      linkList.insertAdjacentHTML('beforeend', button);
    }
+   document.querySelector('.link-list li button').className = 'active';
+   
+   linkList.addEventListener('click', (e) => {
+      if (e.target.type === 'button' ) {
+         document.querySelector('.active').className = '';
+         e.target.className = 'active';
+         showPage(list, e.target.textContent);
+      }
+   });
    
 }
-addPagination(data);
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
